a18e3733da6926e276b60141926e18671ea66b9f
Paul Fruitful <fruitful2007@outlook.com>
2023-02-15 21:07:55 +0100

    Navbar commence

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,8 +3,17 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='grid justify-self-stretch  p-8 shadow-md bg-white'>
-      <a href='' className=' justify-self-start ml-3'><span className='text-3xl font-bold'>JuEats</span></a>
+    <div className='grid justify-self-stretch  p-6 shadow-md bg-white'>
+      <a hrefLang='jolbist.com' className=' justify-self-start ml-3'><span className='text-3xl font-bold'>JuEats</span></a>
+      <div className=' justify-self-end'>
+         <div className='flex flex-row'>
+          
+            <li className='mx-4 list-none font-bold'>About Us</li>
+            <li className='mx-4 list-none font-bold'>Services</li>
+            <li className='mx-4 list-none font-bold'>Menu</li>
+            <li className='mx-4 list-none fon-bold'>Contact Us</li>
+             </div>
+      </div>
     </div>
   )
 }

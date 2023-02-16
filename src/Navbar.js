8a4ad974a19a549c8b4f8ace17c8b45b1dbda5e1
Paul Fruitful <fruitful2007@outlook.com>
2023-02-16 20:16:35 +0100

    Navbar builds

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
-
 const Navbar = () => {
   return (
     <div className='grid justify-self-stretch  p-6 shadow-md bg-white'>
-      <a hrefLang='jolbist.com' className=' justify-self-start'><span className='text-3xl font-bold '>JuEats</span></a>
-      <div className=' justify-self-end'>
-         <div className='flex flex-row' style={{marginTop:"-20px" }}>
+      <a hrefLang='jolbist.com' className=' justify-self-start '><span className='text-3xl font-bold '>JuEats</span></a>
+      <div className=' justify-self-center'>
+         <div className='hidden  lg:flex flex-row' style={{marginTop:"-20px" }}>
           
-            <li className='mx-4 hover:p-3 list-none font-bold'>About Us</li>
-            <li className='mx-4 hover:p-3 list-none font-bold'>Services</li>
-            <li className='mx-4 hover:p-3 list-none font-bold'>Menu</li>
-            <li className='mx-4 hover:p-3 list-none font-bold'>Contact Us</li>
+            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>About Us</li>
+            <li className='mx-4 hover:p-3 hover:text-red-600  list-none font-bold'>Services</li>
+            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Menu</li>
+            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Contact Us</li>
              </div>
+      </div>
+      <div className='flex flex-col lg:hidden absolute top-0 left-0 bg-white shadow-sm' style={{height:'100vh'}}>
+     
+          
+          <li className='m-4 hover:p-3 hover:text-red-600 list-none font-bold'>About Us</li>
+          <li className='m-4 hover:p-3 hover:text-red-600  list-none font-bold'>Services</li>
+          <li className='m-4 hover:p-3 hover:text-red-600 list-none font-bold'>Menu</li>
+          <li className='m-4 hover:p-3 hover:text-red-600 list-none font-bold'>Contact Us</li>
+          
       </div>
     </div>
   )

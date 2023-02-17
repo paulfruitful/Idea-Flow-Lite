a629cdc165576a21320e1df6c712514a0711ead2
Paul Fruitful <fruitful2007@outlook.com>
2023-02-17 19:59:07 +0100

    anchor error

--- src/components/snippets/clickBTN.js
+++ src/components/snippets/clickBTN.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const BTN = ({value}) => {
   return (
     <div className='p-6 relative'>
-      <h1 className='p-6 bg-white rounded text-md text-black font-bold shadow-sm'>{{value}}</h1>
+      <a className='p-6 bg-white rounded text-md text-black font-bold shadow-sm' href={'#'}>{{value}}</a>
     </div>
   )
 }

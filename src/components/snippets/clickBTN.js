ccd1df2681672bbbdc38a34d8f8b3ca5615c0a95
Paul Fruitful <fruitful2007@outlook.com>
2023-02-17 20:20:40 +0100

    Error

--- src/components/snippets/clickBTN.js
+++ src/components/snippets/clickBTN.js
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types'
 const BTN = ({value}) => {
   return (
     <div className='p-6 relative'>
-      <a className='p-6 bg-white rounded text-md text-black font-bold shadow-sm' href={'#'}>{{value}}</a>
-    </div>
+  
+     </div>
   )
 }
 

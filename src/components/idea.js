c6ca7743f299ebc432394ed89fd46d8b17747044
Paul Fruitful <fruitful2007@outlook.com>
2023-02-20 15:07:13 +0100

    Single Idea components

---
+++ src/components/idea.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const idea = ({name}) => {
+  return (
+    <div>
+      <h1>{name}</h1>
+    </div>
+  )
+}
+
+export default idea
--- src/components/ideas.js
+++ src/components/ideas.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import idea from './idea'
 
 const ideas = ({ideas}) => {
   return (
     <div>
       {ideas.forEach(idea => {
-        
+        <idea idea={idea.title}/>
       })}
     </div>
   )

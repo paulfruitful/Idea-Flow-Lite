debbc9c0e0ca599d1bcf0611b064b7fccec6be79
Paul Fruitful <fruitful2007@outlook.com>
2023-02-20 13:58:04 +0100

    Playing around and experimenting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Navbar from "./components/Navbar";
+import { useState } from "react";
 import Hero from "./components/hero";
 import './input.css';
 function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-     <Hero/>
+ const [ideas,setIdeas]=useState('Hello')
 
+const top= async ()=>{
+  const res=await fetch('http://127.0.0.1:8000/api/ideas')
+  const result=res.json()
+  return result
+}
+
+console.log(top())
+
+  return (
+    <div className="App" >
+     <Navbar/>
+     <Hero onClick={()=>setIdeas('Me')}/>
+   <p>{ideas}</p>
     </div>
   );
 }

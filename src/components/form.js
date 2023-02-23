aca2b03fe95558328e5b67e126051c42454d7f04
Paul Fruitful <fruitful2007@outlook.com>
2023-02-23 20:25:34 +0100

    Experiment

--- src/components/form.js
+++ src/components/form.js
@@ -5,7 +5,7 @@ const form = () => {
     <div>
       <form className='grid  p-6 shadow-sm  justify-items-center '>
         <div className='flex flex-col justify-self-center'>
-
+            <div className='p-6 mt-3 flex flex-row'><label className='font-bold text-md'>Write Your Hero</label> <input name='hero' placeholder='Best Restaurant'></input></div>
         </div>
       </form>
     </div>

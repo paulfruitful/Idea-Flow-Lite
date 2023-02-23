3fd76658cc9263af4f65615e9174322226d78150
Paul Fruitful <fruitful2007@outlook.com>
2023-02-23 21:43:55 +0100

    Form component

--- src/components/form.js
+++ src/components/form.js
@@ -7,7 +7,7 @@ const Form = () => {
     <div>
       <form className='grid  p-6 shadow-sm  justify-items-center '>
         <div className='flex flex-col justify-self-center'>
-            <div className='p-6 mt-3 flex flex-row'><label className='font-bold text-md m-4'>Write Your Hero</label> <input name='hero' placeholder='Best Restaurant'></input> <button className='p-6 bg-black text-white font-bold text-md' value={'Submit'}></button></div>
+            <div className='p-6 mt-3 flex flex-row'><label className='font-bold text-md m-4'>Write Your Hero</label> <input name='hero' placeholder='Best Restaurant'></input> <button className='p-2 rounded-md bg-black text-white font-bold text-sm' value={'Submit'}>Submit</button></div>
         </div>
       </form>
     </div>

649fff50db8ce3dabcc031b8707b9cdff78967c1
Paul Fruitful <fruitful2007@outlook.com>
2023-02-24 19:56:36 +0100

    Hero change

--- src/components/snippets/clickBTN.js
+++ src/components/snippets/clickBTN.js
@@ -3,7 +3,7 @@ import React from 'react'
 const BTN = ({value}) => {
   return (
     <div className='p-6 relative'>
-  
+      <a className='py-3 px-2 rounded-md bg-white text-black font-bold text-sm'>{value}</a>
      </div>
   )
 }

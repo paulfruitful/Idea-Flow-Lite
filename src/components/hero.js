526cff2082cc3de5354aa469351756b61a704add
Paul Fruitful <fruitful2007@outlook.com>
2023-02-24 21:25:32 +0100

    twk

--- src/components/hero.js
+++ src/components/hero.js
@@ -6,8 +6,8 @@ const Hero = ({text}) => {
       <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style={{height:'400px'}}>
         <div class=" justify-self-center m-auto">
            <div class="m-auto flex flex-col">
-             <p class="text-3xl lg:text-5xl text-white m-auto font-bold "><span class="">Let Your</span>  Ideas Flow </p>
-             <p class="text-xl text-center lg:flex lg:flex-row  font-bold p-6 mb-4" style={{color:' #ffffff'}}>Share Your Ideas <span class="hidden lg:flex">, Offer Your Solutions and Report Your Problems</span> </p>
+             <p class="text-3xl lg:text-5xl text-white m-auto font-bold ">Get Inspired</p>
+             <p class="text-xl text-center lg:flex lg:flex-row  p-6 mb-4" style={{color:' #ffffff'}}>By Reading Ideas, Solutions and Problems </p>
             
            </div>
         </div>

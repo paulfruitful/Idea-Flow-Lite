8952a2953c468157454d1a8f9520771a890d4abe
Paul Fruitful <fruitful2007@outlook.com>
2023-02-24 19:36:36 +0100

    NAvbar tweaks

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,18 +7,18 @@ const Navbar = () => {
       <div className=' justify-self-center'>
          <div className='hidden  lg:flex flex-row' style={{marginTop:"-20px" }}>
           
-            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Ideas</li>
-            <li className='mx-4 hover:p-3 hover:text-red-600  list-none font-bold'>Solutions</li>
-            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Problems</li>
+            <li className='mx-4 hover:p-3 hover:text-blue-600 list-none font-bold'>Ideas</li>
+            <li className='mx-4 hover:p-3 hover:text-blue-600  list-none font-bold'>Solutions</li>
+            <li className='mx-4 hover:p-3 hover:text-blue-600 list-none font-bold'>Problems</li>
            
              </div>
       </div>
       <div className='flex flex-col lg:hidden absolute top-0 left-0 bg-white shadow-sm' style={{height:'100vh',display:'none'}}>
      
           
-      <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Ideas</li>
-            <li className='mx-4 hover:p-3 hover:text-red-600  list-none font-bold'>Solutions</li>
-            <li className='mx-4 hover:p-3 hover:text-red-600 list-none font-bold'>Problems</li>
+      <li className='mx-4 hover:p-3 hover:text-blue-600 list-none font-bold'>Ideas</li>
+            <li className='mx-4 hover:p-3 hover:text-blue-600  list-none font-bold'>Solutions</li>
+            <li className='mx-4 hover:p-3 hover:text-blue-600 list-none font-bold'>Problems</li>
           
       </div>
     </div>

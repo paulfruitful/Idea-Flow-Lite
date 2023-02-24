cab17db4708bb2623f2fb406fe2d7b2249e9e93f
Paul Fruitful <fruitful2007@outlook.com>
2023-02-24 21:28:26 +0100

    twk and chng

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import BTN from './snippets/clickBTN'
 const Hero = ({text}) => {
   return (
  <div>
-      <div class="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style={{height:'400px'}}>
-        <div class=" justify-self-center m-auto">
-           <div class="m-auto flex flex-col">
-             <p class="text-3xl lg:text-5xl text-white m-auto font-bold ">Get Inspired</p>
-             <p class="text-xl text-center lg:flex lg:flex-row  p-6 mb-4" style={{color:' #ffffff'}}>By Reading Ideas, Solutions and Problems </p>
+      <div className="grid justify-self-stretch justify-items-center mt-3 mb-6  bg-gradient-to-r from-cyan-500 to-blue-700 w-full" style={{height:'400px'}}>
+        <div className=" justify-self-center m-auto">
+           <div className="m-auto flex flex-col">
+             <p className="text-3xl lg:text-5xl text-white m-auto font-bold ">Get Inspired</p>
+             <p className="text-xl text-center lg:flex lg:flex-row  p-6 mb-4" style={{color:' #ffffff'}}>By Reading Ideas, Solutions and Problems </p>
             
            </div>
         </div>

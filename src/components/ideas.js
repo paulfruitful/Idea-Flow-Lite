97e4518d5fe5b0b1b39f0f9334ba89b0ce9b4782
Paul Fruitful <fruitful2007@outlook.com>
2023-04-24 14:37:24 +0100

    Struggling with errors

--- src/components/ideas.js
+++ src/components/ideas.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Idea from './idea'
-import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 
@@ -12,12 +11,8 @@ const Ideas = ({ideas}) => {
     <div className='grid justify-items-center p-6 m-6'>
       <h1 className='text-3xl font-bold p-3'>Idea News</h1>
       <div className='flex flex-row flex-wrap'>
-        {
-        ideas?
-          ideas.map( (element) => {
-              
-              return <Link to={`/ideas/${element.id}`}> <Idea name={element.title} key={element.id} author={element.author} tagline={element.tagline}/></Link>            }) : 'No Idea'
-}
+        {ideas?console.log(ideas[0]):console.log('Failed')}
+      
       </div>
       
               

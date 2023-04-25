a64e742ee1c2ffab85e2dd18a9b796f386b4bb7f
Paul Fruitful <fruitful2007@outlook.com>
2023-04-25 22:16:47 +0100

    refact

--- src/components/snippets/Error.js
+++ src/components/snippets/Error.js
@@ -2,9 +2,9 @@
 
 const Error = ({message}) => {
   return (
-    <div className="justify-self-center m-12 p-6 object-center">
-      <div className="text-red-600 font-bold text-2xl "><span className="text-center">{message}</span></div>
-    </div>
+    <div className="justify-self-center flex flex-col m-12 p-6 object-center">
+      <img src="https://www.replaylistings.com/assets/images/result-not-found-1.png"></img>
+       </div>
   )
 }
 

8255c3f9c126ab05c3a127707788461901dbf451
Paul Fruitful <fruitful2007@outlook.com>
2023-04-25 21:45:48 +0100

    Error snippet

--- src/components/ideas.js
+++ src/components/ideas.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Idea from './idea'
+import Error from './snippets/Error'
 import { Link } from 'react-router-dom'
 
 
 
 
 const Ideas = ({ideas}) => {
-   let id=0
+
   return (
     <div className='grid justify-items-center p-6 m-6'>
       <h1 className='text-3xl font-bold p-3'>Idea News</h1>
       <div className='flex flex-row flex-wrap'>
-        {ideas?ideas.map(idea=><Idea key={idea.id} name={idea.name} author={idea.author} tagline={idea.tagline}/>):'No Ideas'}
+        {ideas?ideas.map(idea=><Idea key={idea.id} name={idea.name} author={idea.author} tagline={idea.tagline}/>): <Error message={'No Ideas'}/>}
       
       </div>
       

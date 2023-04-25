2f4481fb3f9f4af5fb2edc3ab1a8e0cd3acc66f0
Paul Fruitful <fruitful2007@outlook.com>
2023-04-25 21:39:01 +0100

    Error fixed

--- src/components/ideas.js
+++ src/components/ideas.js
@@ -11,7 +11,7 @@ const Ideas = ({ideas}) => {
     <div className='grid justify-items-center p-6 m-6'>
       <h1 className='text-3xl font-bold p-3'>Idea News</h1>
       <div className='flex flex-row flex-wrap'>
-        {ideas?console.log(Array.isArray(ideas)):'No Ideas'}
+        {ideas?ideas.map(idea=><Idea key={idea.id} name={idea.name} author={idea.author} tagline={idea.tagline}/>):'No Ideas'}
       
       </div>
       

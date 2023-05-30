5a559d22eb5305bcb3dc34f1039e1a4058d39790
Paul Fruitful <fruitful2007@outlook.com>
2023-05-30 22:51:10 +0100

    ++

--- src/components/Solution.js
+++ src/components/Solution.js
@@ -3,6 +3,13 @@ import { useParams } from 'react-router-dom'
 
 const Solution=()=>{
 const [solution,setSolution]=useState([])
+const {id}=useParams()
+
+useEffect(async()=>{
+    const fetch=await fetch()
+    const res=await fetch.json()
+    setSolution(res)
+},[])
 
 
 }

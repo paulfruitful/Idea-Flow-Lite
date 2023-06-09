48d11d3d9cc87f70d028ae304ddd3e4ad85cf53f
Paul Fruitful <fruitful2007@outlook.com>
2023-06-09 22:29:25 +0100

    rennovations

--- src/components/ideaPool.js
+++ src/components/ideaPool.js
@@ -4,20 +4,21 @@ import Idea from './idea'
 import { useContext } from 'react'
 import { DataContext } from '../App'
 import { Link } from 'react-router-dom'
+import {Error} from '../components/snippets/'
 const IdeaPool = () => {
 const {ideas}=useContext(DataContext)
   return (
     <div>
      <Hero/>
      <div className='grid justify-items-center p-6 m-6'>
-     <h1 className='text-3xl font-bold p-3'>Idea News</h1>
+     <h1 className='text-3xl font-bold p-3'>Ideas Inspire Creativity</h1>
       <div className='flex flex-row flex-wrap'>
         {
         ideas?
           ideas.map( (element) => {
               
               return <Link to={`/ideas/${element.id}`}> <Idea name={element.title} key={element.id} author={element.author} tagline={element.tagline}/> </Link>
-            }) : 'No Idea'
+            }) : <Error message="No Ideas Available"/>
 }
       </div>
       </div>

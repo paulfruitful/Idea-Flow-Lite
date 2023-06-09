b3dc2d3f3408b9cf297555d3bd36a4ef2ff6d642
Paul Fruitful <fruitful2007@outlook.com>
2023-06-09 22:19:49 +0100

    Modifications and rennovations

--- src/components/idea.js
+++ src/components/idea.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Idea = ({name,author,tagline}) => {
+const Idea = ({id,name,author,tagline}) => {
+  const link=`/ideas/${id}`
   return (
+    <Link to={link}>
     <div className='grid justify-self-center m-6 p-12 shadow-lg rounded-sm'>
      <div className='grid justify-self-stretch'>
       <h1 className='justify-self-start text-blue-600 text-md'>{tagline}</h1>
@@ -9,6 +12,7 @@ const Idea = ({name,author,tagline}) => {
       <h1 className='p-3 font-bold text-sm justify-self-end'>By {author}</h1>
       </div>
     </div>
+    </Link>
   )
 }
 
--- src/components/ideas.js
+++ src/components/ideas.js
@@ -12,7 +12,7 @@ const Ideas = ({ideas}) => {
     <div className='grid justify-items-center p-6 m-6'>
       <h1 className='text-3xl font-bold p-3'>Idea News</h1>
       <div className='flex flex-row flex-wrap'>
-        {ideas?ideas.map(idea=><Idea key={idea.id} name={idea.name} author={idea.author} tagline={idea.tagline}/>): <Error message={'No Ideas'}/>}
+        {ideas?ideas.map(idea=><Idea key={idea.id} id={idea.id} name={idea.name} author={idea.author} tagline={idea.tagline}/>): <Error message={'No Ideas'}/>}
       
       </div>
       

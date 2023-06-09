c7cf387efe9d066494e7dbf6399eae83981d9569
Paul Fruitful <fruitful2007@outlook.com>
2023-06-09 22:31:39 +0100

    patches and changes

--- src/components/ideaPool.js
+++ src/components/ideaPool.js
@@ -4,12 +4,12 @@ import Idea from './idea'
 import { useContext } from 'react'
 import { DataContext } from '../App'
 import { Link } from 'react-router-dom'
-import {Error} from '../components/snippets/'
+import Error from './snippets/Error'
 const IdeaPool = () => {
 const {ideas}=useContext(DataContext)
   return (
     <div>
-     <Hero/>
+    
      <div className='grid justify-items-center p-6 m-6'>
      <h1 className='text-3xl font-bold p-3'>Ideas Inspire Creativity</h1>
       <div className='flex flex-row flex-wrap'>
